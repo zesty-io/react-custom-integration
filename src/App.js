981403039658725a8b140ab2ebb903844b01601d
ardeay <apuzzo@gmail.com>
2023-07-06 19:43:07 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { toJSON } from '@zesty-io/webengine-json';
+import ZestyConfig from "./zesty.config.json";
+import React from 'react';
+
 
 function App() {
+  // setup state to capture content
+  const [content, setContent] = React.useState(false);
+
+  // use effect to fetch the data
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const data = await toJSON(ZestyConfig.stage,'about/');
+      setContent(data);
+    }
+    fetchData();
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
+        {content && <h1>{content.title}</h1>}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {ZestyConfig.stage} Edit <code>src/App.js</code> and save to reload.
+          
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
